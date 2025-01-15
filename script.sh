965bbb4dc5aa5df14ab1fc7dac60e2dfd15830f6
Kilian Bachmann <KilianBachmann@googlemail.com>
2025-01-15 15:57:41 +0100

    Change script to dynamically slow the song down four times

--- script.sh
+++ script.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Function to check if a file is already converted
-is_converted() {
-  local filename="$1"
-  if [[ $filename =~ ^.+\.[0-9]+%_slowdown\..+$ ]]; then
-    return 0  # File is converted
-  else
-    return 1  # File is not converted
-  fi
-}
+if [ ! -d "_input" ]; then
+  echo "There is no \"_input\" folder."
+  echo "Creating \"_input\" folder..."
+  mkdir _input
+  echo "Folder created. Create a folder within it, with the artists name and place any songs in the artists folder."
+  echo "The Song should have the naming convention \"songName_123bpm\"."
+  echo "The pbm number will be used to convert it to slower tempos."
+  echo "It should look something like this: i.e. \"_input/Tool/Bottom_140bpm.m4a\""
+  exit 0
+fi
 
 # Loop through files in the current directory
 for file in _input/*/* ; do
@@ -23,24 +24,19 @@ for file in _input/*/* ; do
   output_filename="${file_without_bpm}_${original_bpm}bpm.mp3"
   output_filename=$(echo $output_filename | sed "s/_input/_output/")
   ffmpeg -y -i $file $output_filename
-  while (( $bpm >= 70 ))
+
+  bpm=$((bpm-(bpm%10)))
+  # echo $bpm
+
+  for i in {1..4}
   do
-    if (( $bpm % 10 > 0 ))
-    then
-      bpm=$((bpm - 1))
-
-      # echo $bpm
-    else
-      ratio=$(bc -l <<< $bpm/$original_bpm)
-      echo "bpm: $bpm || ratio: $ratio"
-      output_filename="${file_without_bpm}_${bpm}bpm.mp3"
-      output_filename=$(echo $output_filename | sed "s/_input/_output/")
-      echo $output_filename
-      ffmpeg -y -i $file -af atempo=$ratio $output_filename
-      bpm=$((bpm - 1))
-      # echo $ratio
-      # echo $bpm
-
-    fi
+    # echo $bpm
+    ratio=$(bc -l <<< $bpm/$original_bpm)
+    # echo "bpm: $bpm || ratio: $ratio"
+    output_filename="${file_without_bpm}_${bpm}bpm.mp3"
+    output_filename=$(echo $output_filename | sed "s/_input/_output/")
+    # echo $output_filename
+    ffmpeg -y -i $file -af atempo=$ratio $output_filename
+    bpm=$((bpm-10))
   done
 done
